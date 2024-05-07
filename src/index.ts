e914bd38f23a06314e66ad46879f59bd535c5b34
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2024-05-07 20:20:45 +0530

    Added Eslint method and routes & controller

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import  express from "express";
-import serverConfig from "./config/serverConfig"
-const app = express()
+import express, { Express } from "express";
 
-app.listen( serverConfig.PORT, ()=> {
-    console.log(`Server started at *:${serverConfig.PORT}`);
-    console.log("wow");
-    
-    
-})
+import serverConfig from "./config/serverConfig";
+import apiRouter from "./routes";
+
+const app: Express = express();
+
+app.use('/api', apiRouter);
+
+app.listen(serverConfig.PORT, () => {
+  
+  console.log(`Server started at *:${serverConfig.PORT}`);
+  console.log("wow");
+});
+
+
