284bd4d2be51fc4bdc437e490dfc243e40b84a3e
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2024-12-09 13:13:42 +0530

    create second docker container for Java code

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import bodyParser from 'body-parser';
 import express, { Express } from "express";
 
 import serverConfig from "./config/serverConfig";
-import runPython from "./containers/runPythonDocker";
+import runJava from './containers/runJavaDocker';
+//import runPython from "./containers/runPythonDocker";
 // import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
 import SampleWorker from "./workers/SampleWorker";
@@ -29,17 +30,34 @@ app.listen(serverConfig.PORT, () => {
 //     location: "Remote | BLR | LKO"
 // }, 1);
 
+// const code = `
+// x = input()
+// y = input()
+// print("value of x is", x)
+// print("value of y is", y)
+// `;
+
+// const inputCase = `100
+// 200`;               
+
+// runPython(code, inputCase);
 const code = `
-x = input()
-y = input()
-print("value of x is", x)
-print("value of y is", y)
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        int input = src.nextInt();
+        System.out.println("input value given by user: " + input);
+        for (int i = 0; i < input; i++) {
+            System.out.println(i);
+        }
+    }
+}
 `;
 
-const inputCase = `100
-200`;               
+const inputCase = `10`;
 
-runPython(code, inputCase);
+runJava(code, inputCase);
 
 });
 
